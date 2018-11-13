73f30ae987063eb2376c9e60da63b0113938b6b4
Claudio Spiess <claudiosv@users.noreply.github.com>
2018-11-13 20:49:54 +0100

    Merge changes

--- photo-stack-front/src/index.js
+++ photo-stack-front/src/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import Login from './Login';
+// import './index.css';
+ReactDOM.render(
+    <Login />,
+  document.getElementById('root')
+);
