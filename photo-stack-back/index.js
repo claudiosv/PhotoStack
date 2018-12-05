ea0c884ee881f8e5712b56df0e04f81e899fbe8f
Riccardo Felluga <rfelluga@unibz.it>
2018-12-05 16:03:27 +0100

    dokcer node dev back end setup

---
+++ photo-stack-back/index.js
@@ -0,0 +1,13 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+server.listen(3000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:3000/");
